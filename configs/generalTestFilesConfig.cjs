05d7c3cfb72d7122b106f00de3c0784ab99db08c
Chamlai <31847168+VeiveHata@users.noreply.github.com>
2024-11-27 09:26:48 +0100

    Error for prefer-screen-queries, no-render-in-lifecycle [THC-387] (#26)

--- configs/generalTestFilesConfig.cjs
+++ configs/generalTestFilesConfig.cjs
@@ -5,8 +5,8 @@ module.exports = {
   files: TEST_FILES_PATTERNS,
 
   rules: {
-    'testing-library/prefer-screen-queries': OFF,
-    'testing-library/no-render-in-lifecycle': OFF,
+    'testing-library/prefer-screen-queries': ERROR,
+    'testing-library/no-render-in-lifecycle': ERROR,
     'testing-library/no-node-access': OFF,
     'testing-library/no-container': OFF,
     'testing-library/prefer-presence-queries': OFF,
